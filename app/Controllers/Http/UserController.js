26ff45a6194c7ff9f84fffb626cdec57cc045129
Goufix <alifer.17.11@gmail.com>
2020-01-29 21:12:31 -0300

    Alterada a query de consulta para nao mostrar a senha

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,16 +1,10 @@
 "use strict";
 
 const User = use("App/Models/User");
-const Database = use("Database");
 
 class UserController {
   async index({ response }) {
-    const users = await User.all();
-    response.status(200).send(users);
-  }
-  async show({ params, response }) {
-    const username = params.username;
-    const user = await Database.table("users")
+    const users = await User.query()
       .select([
         "id",
         "username",
@@ -20,13 +14,31 @@ class UserController {
         "work_shift",
         "points"
       ])
-      .where("username", username);
+      .fetch();
+    response.status(200).send(users);
+  }
+
+  async show({ params, response }) {
+    const username = params.username;
+    const user = await User.query()
+      .select([
+        "id",
+        "username",
+        "email",
+        "level",
+        "github_username",
+        "work_shift",
+        "points"
+      ])
+      .where("username", username)
+      .fetch();
     if (user) {
       return user;
     } else {
       response.status(404).send({ error: "Usuário não encontrado" });
     }
   }
+
   async create({ request, response }) {
     const userRequestData = request.only([
       "username",
