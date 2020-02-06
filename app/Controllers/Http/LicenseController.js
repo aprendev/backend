9685bb29941912ce2ef71794504e85dcb6b4bd9c
Goufix <alifer.17.11@gmail.com>
2020-02-05 22:17:52 -0300

    Adiciona o método para criar uma licença

--- app/Controllers/Http/LicenseController.js
+++ app/Controllers/Http/LicenseController.js
@@ -6,16 +6,52 @@
  * Resourceful controller for interacting with licenses
  */
 class LicenseController {
-  /* TODO:
-    Criar os métodos para inclusão e alteração de licenças.
-
-    Regras de negócio:
-     - Um usuário só pode ter uma licença ativa
-     - Um usuário deve ficar em licença por no mínimo 7 dias e no máximo 30
-     - O usuário que não solicitar a baixa da licença dentro do prazo L + 1, será automaticamente desativado.
-     - A solicitação de licença deve possuir a autorização de um instrutor ou admin
-  */
-  // Todo: Goufix
+  async create({ auth, request, response }) {
+    const { days, authorization_id: authorizationId } = request.all();
+    const date = new Date();
+    date.setDate(date.getDate() + days);
+    const authorizer = await User.findOrFail(authorizationId);
+    const authorizerLevel = await authorizer.level().fetch();
+    if (authorizerLevel.id === 3 || authorizerLevel.id === 2) {
+      if (days < 7 || days > 30) {
+        throw new HttpException('Período de licença inválido');
+      }
+      const user = await auth.getUser();
+      const { id } = user;
+      const userLicences = await Database.select([
+        'L.id',
+        'L.user_id',
+        'L.created_at',
+        'L.ends_in',
+        'L.authorization_id',
+        'U.id as user_id',
+        'U.username as user_name'
+      ])
+        .from('licenses as L')
+        .innerJoin('users as U', 'L.user_id', '=', 'U.id')
+        .where({ user_id: id });
+      if (userLicences.length !== 0) {
+        throw new HttpException(
+          'Usuário já possui licença ativa',
+          409,
+          'ERR_LICENSE_EXISTS'
+        );
+      } else {
+        const license = await Licence.create({
+          user_id: user.id,
+          ends_in: date,
+          authorization_id: authorizationId
+        });
+        response.status(200).json({ license });
+      }
+    } else {
+      throw new HttpException(
+        'O usuário que autorizou a licença não é um instrutor',
+        403,
+        'ERR_INVALID_AUTHORIZER'
+      );
+    }
+  }
 }
 
 module.exports = LicenseController;
