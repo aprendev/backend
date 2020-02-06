4a5eb7bbb488097e9568f58242854cd5cebf332e
Goufix <alifer.17.11@gmail.com>
2020-02-05 22:15:51 -0300

    Formata o model

--- app/Models/License.js
+++ app/Models/License.js
@@ -1,9 +1,6 @@
-'use strict'
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
-class License extends Model {
-}
+class License extends Model {}
 
-module.exports = License
+module.exports = License;
