b8f4074f7e2cfc271e97c0390a2e628549cd88b2
Goufix <alifer.17.11@gmail.com>
2020-02-06 20:29:41 -0300

    Adiciona novos campos na tabela de desafios

--- database/migrations/1581010464338_challenges_schema.js
+++ database/migrations/1581010464338_challenges_schema.js
@@ -5,11 +5,18 @@ class ChallengesSchema extends Schema {
   up() {
     this.create('challenges', (table) => {
       table.increments();
+      table.string('description').notNullable();
+      table.string('title').notNullable();
       table
         .integer('level_id')
         .unsigned()
         .references('id')
         .inTable('levels');
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users');
       table.timestamp('ends_in').notNullable();
       table.timestamps();
     });
