de0c8504364e6af293e554c04907a854db827167
Goufix <alifer.17.11@gmail.com>
2020-02-06 15:09:43 -0300

    Eslint + Prettier

--- app/Models/Challenge.js
+++ app/Models/Challenge.js
@@ -1,9 +1,6 @@
-'use strict'
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
-class Challenge extends Model {
-}
+class Challenge extends Model {}
 
-module.exports = Challenge
+module.exports = Challenge;
